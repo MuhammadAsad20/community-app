0cfa7ff2f01612ad2c2b7fdfb5d432e57637c2f6
MuhammadAsad20 <121595889+MuhammadAsad20@users.noreply.github.com>
2025-12-24 19:19:15 +0500

    Add files via upload

---
+++ src/app/Files/page.js
@@ -0,0 +1,10 @@
+import FileShare from '@/components/FileShare'
+
+export default function Files() {
+  return (
+    <main className="min-h-screen pt-24 ">
+
+      <FileShare />
+    </main>
+  )
+}
