622a17e1271ebbaef00413b318e0b1529129da4f
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2025-12-24 09:15:21 -0800

    Update page.js

--- src/app/Files/page.js
+++ src/app/Files/page.js
@@ -1,10 +1,10 @@
-import FileShare from '@/components/FileShare'
+// import FileShare from '@/components/FileShare'
 
 export default function Files() {
   return (
     <main className="min-h-screen pt-24 ">
 
-      <FileShare />
+      saklj'slakjdksakjSAKja
     </main>
   )
 }
